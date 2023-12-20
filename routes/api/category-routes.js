e85107641a949ed2bc81461506890107ff81f9fb
Amanuel <amanueleman@gmail.com>
2023-12-19 23:33:16 -0500

    GET, PUT, POST and DELETE routes are created on product route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -9,7 +9,13 @@ router.get('/', async (req, res) => {
     // find all categories
     const categoryData = await Category.findAll({
       // be sure to include its associated Products
-      include: [{ model: Category  }, { model: Product }],
+      attributes: ['id', 'category_name'],
+      include: [
+        {          
+          model: Product,
+          attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }
+      ],
     });
     res.status(200).json(categoryData);
   } catch (err) {
@@ -22,11 +28,17 @@ router.get('/:id', async (req, res) => {
     // find one category by its `id` value
     const categoryData = await Category.findBypk(req.params.id, {
       // be sure to include its associated Products
-      include: [{ model: Category }, { model: Product }],
+      attributes: ['id', 'category_name'],
+      include: [
+        { 
+          model: Product,
+          attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+         }
+        ],
     });
-    
-    if (!categoryData) { 
-      res.status(404).json({ message: 'No driver found with that id!' });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
       return;
     }
 
@@ -38,7 +50,7 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new category
-  try { 
+  try {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (err) {
